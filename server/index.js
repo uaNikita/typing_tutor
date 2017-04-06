d86d881457cf0a996a49124843e06975a7c05387
Nikita Zhukov <nikita.zhukov@gmail.com>
2017-04-06 20:12:14 +0300

    migrate from Django to Express

---
+++ server/index.js
@@ -0,0 +1,52 @@
+let path = require('path');
+let express = require('express');
+let bodyParser = require('body-parser');
+let cookieParser = require('cookie-parser');
+let session = require('express-session');
+let mongoose = require('mongoose');
+let config = require('config');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.get('database'));
+mongoose.connection.on('error', () => {
+   throw new Error(`unable to connect to database: ${config.db}`);
+});
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(session({
+   secret: 'white cat',
+   resave: false,
+   saveUninitialized: false
+}));
+
+// Passport
+require('./passport')(app);
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+   res.render('index', { title: 'Express' });
+});
+
+
+app.get('/test-page', function(req, res) {
+   res.render('test-page');
+});
+
+let routes = require('./routes/index');
+app.use('/api', routes);
+
+
+// Errors
+require('./errors')(app);
+
+app.listen(3000);
