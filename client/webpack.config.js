0a725159063dce792428d05a7a6243cc01204e5c
Nikita Zhukov <nikita.zhukov@gmail.com>
2017-06-30 18:12:48 +0300

    changed

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,148 +6,138 @@ var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var env = process.env.WEBPACK_ENV;
+var isProduction = process.env.WEBPACK_ENV === 'production' ? true : false;
 
 let config = {
-   context: path.join(__dirname),
+  context: path.join(__dirname),
 
-   // The entry point for the bundle.
-   entry: './entry.jsx',
+  // The entry point for the bundle.
+  entry: './entry.jsx',
 
-   // Options affecting the output.
-   output: {
-      path: path.join(__dirname, 'dist'),
-      // The filename of the entry chunk as relative path inside the output.path directory.
-      filename: 'main.js'
-   },
+  // Options affecting the output.
+  output: {
+    path: path.join(__dirname, 'dist'),
+    // The filename of the entry chunk as relative path inside the output.path directory.
+    filename: 'main.js'
+  },
 
-   watchOptions: {
-      aggregateTimeout: 100
-   },
+  watchOptions: {
+    aggregateTimeout: 100
+  },
 
-   devtool: 'eval',
+  devtool: isProduction ? 'source-map' : 'eval',
 
-   resolve: {
-      alias: {
-         Blocks: path.resolve(__dirname, 'blocks/'),
-         Redux: path.resolve(__dirname, 'redux/')
+  resolve: {
+    alias: {
+      Blocks: path.resolve(__dirname, 'blocks/'),
+      Redux: path.resolve(__dirname, 'redux/')
+    }
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+  ],
+
+  // Options affecting the normal modules
+  module: {
+    // A array of automatically applied loaders.
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          plugins: ['transform-object-rest-spread'],
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                url: true,
+                import: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            },
+            'stylus-loader'
+          ]
+        })
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }, {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: '10000',
+          mimetype: 'application/font-woff',
+          name: '[name].[ext]'
+        }
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: '10000',
+          mimetype: 'application/font-woff',
+          name: '[name].[ext]'
+        }
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: '10000',
+          mimetype: 'application/octet-stream',
+          name: '[name].[ext]'
+        }
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]'
+        }
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: '10000',
+          mimetype: 'image/svg+xml',
+          name: '[name].[ext]'
+        }
       }
-   },
-
-   // Options affecting the normal modules
-   module: {
-      // A array of automatically applied loaders.
-      rules: [
-         {
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-               plugins: ['transform-object-rest-spread'],
-               presets: ['es2015', 'react']
-            }
-         },
-         {
-            test: /\.styl$/,
-            loader: ExtractTextPlugin.extract({
-               fallback: 'style-loader',
-               use: [
-                  {
-                     loader: 'css-loader',
-                     options: {
-                        url: true,
-                        import: true
-                     }
-                  },
-                  'postcss-loader',
-                  'stylus-loader'
-
-               ]
-            })
-         }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract({
-               fallback: 'style-loader',
-               use: 'css-loader'
-            })
-         }, {
-            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            query: {
-               limit: '10000',
-               mimetype: 'application/font-woff',
-               name: '[name].[ext]'
-            }
-         }, {
-            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            query: {
-               limit: '10000',
-               mimetype: 'application/font-woff',
-               name: '[name].[ext]'
-            }
-         }, {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            query: {
-               limit: '10000',
-               mimetype: 'application/octet-stream',
-               name: '[name].[ext]'
-            }
-         }, {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'file-loader',
-            query: {
-               name: '[name].[ext]'
-            }
-         }, {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader',
-            query: {
-               limit: '10000',
-               mimetype: 'image/svg+xml',
-               name: '[name].[ext]'
-            }
-         }
-      ]
-   }
+    ]
+  }
 
 };
 
-let plugins = [
-   new ExtractTextPlugin('[name].css'),
-   new webpack.LoaderOptionsPlugin({
-      options: {
-         context: __dirname,
-         postcss: [
-            autoprefixer
-         ]
+if (isProduction) {
+  config.plugins = config.plugins.concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
       }
-   })
-];
-
-if (env === 'production') {
-
-   plugins = plugins.concat([
-      new webpack.DefinePlugin({
-         'process.env': {
-            'NODE_ENV': JSON.stringify('production')
-         }
-      }),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-         compressor: {
-            warnings: false
-         }
-      })
-   ]);
-
-   _.merge(config, {
-      devtool: 'source-map'
-   });
-
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]);
 }
-
-config.plugins = plugins;
 
 module.exports = config;
