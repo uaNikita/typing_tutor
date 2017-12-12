017bdc321863f0ce78c37f9cad10a9fc166e2be0
uaNikita <nikita.zhukov@gmail.com>
2017-12-12 17:44:19 +0200

    updated addText request

--- server/controllers/modes/text.js
+++ server/controllers/modes/text.js
@@ -1,6 +1,6 @@
+const crypto = require('crypto');
 const httpStatus = require('http-status');
 
-const APIError = require('../utils/APIError');
 const User = require('../models/user');
 
 const addText = (req, res, next) => {
@@ -12,17 +12,21 @@ const addText = (req, res, next) => {
     }
   } = req;
 
-  User.get(req.user.id)
+  User.get(id)
     .then(user => {
-      user.modes.text.entities.push({
-        id: 1,
+      const entity = {
+        id: crypto.randomBytes(40).toString('hex'),
         typed: '',
         last: text,
-      });
+      }
 
-      console.log(user.get('modes.text'));
+      user.modes.text.entities.push(entity);
 
-      res.json(httpStatus[200]);
+      if (select) {
+        user.modes.selectedId = entity.id;
+      }
+
+      return user.save().then(() => res.json(entity.id));
     })
     .catch(e => next(e));
 }
