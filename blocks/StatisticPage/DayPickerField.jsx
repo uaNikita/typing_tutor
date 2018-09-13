83ea1a021399b52b3f454ad01f863afda1c045df
Unknown <nikita.zhukov@gmail.com>
2018-09-13 21:34:58 +0300

    add show/hide logic to DayPickerField

--- blocks/StatisticPage/DayPickerField.jsx
+++ blocks/StatisticPage/DayPickerField.jsx
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
 import { Field } from 'redux-form/immutable';
+import classNames from 'classnames';
 
 import DayPicker from 'react-day-picker';
 
 import styles from './statistic.module.styl';
 
 class Block extends Component {
-  handleFocus() {
-    console.log(111);
-  }
+  state = {
+    show: false,
+  };
 
-  handleDayClick() {
-    console.log(987987);
-  }
+  handleFocus = () => {
+    console.log('handleFocus');
+    this.setState({
+      show: true,
+    });
+  };
+
+  handleDayClick = (...args) => {
+    console.log('handleDayClick', args);
+
+    this.setState({
+      show: false,
+    });
+  };
 
   render() {
-    const { props } = this;
+    const {
+      props,
+      state: {
+        show,
+      },
+    } = this;
+
+    const dayPickerOverlayStyleName = classNames(
+      'overlay',
+      {
+        show,
+      },
+    );
 
     return (
       <div styleName="daypicker">
         <Field
           {...props}
-            onFocus={this.handleFocus}
+          onFocus={this.handleFocus}
         />
 
-        <div className="DayPickerInput-Overlay" styleName="">
-          <DayPicker
-            onDayClick={this.handleDayClick}
-          />
+        <div className="DayPickerInput-Overlay" styleName={dayPickerOverlayStyleName}>
+          <DayPicker onDayClick={this.handleDayClick} />
         </div>
       </div>
     );
