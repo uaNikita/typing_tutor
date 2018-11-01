cbcbd3a3e2f3c84856257e9458abf2f98405cd7e
uaNikita <nikita.zhukov@gmail.com>
2018-11-01 15:24:58 +0200

    add more description to help page

--- blocks/Help/component.jsx
+++ blocks/Help/component.jsx
@@ -1,23 +1,31 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
 
 import styles from './help.module.styl';
 
 const Block = () => (
-  /* eslint-disable react/jsx-one-expression-per-line */
-  <Fragment>
-    <h1 styleName="h1">Help</h1>
-    <h2 styleName="h2">Modes</h2>
+  /* eslint-disable react/jsx-one-expression-per-line, max-len */
+  <section styleName="help">
+    <h1>Help</h1>
+    <em>Touch to type</em> is app which help you to typing text faster with smaller amount of typos. You can use ts without registration, but in this case your progress will be kept in your browser. So if for example you move to another browser or clear your browser data you lose all your statistic. If you want to prevent this, please create your account here <Link to="/authorization/sign-up" />
 
-    <p><strong>Text</strong>. Text mode description
-    </p>
-    <p><strong>Learning</strong>. Applicaton has two differents submodes for learning mode:</p>
+    <h2>Modes</h2>
+    The app has two modes.
+    <p><strong>Text</strong>. Text mode description</p>
+    <p><strong>Learning</strong>. Which has two different submodes for learning mode:</p>
     <ul>
-      <li>Fingers</li>
-      <li>Free</li>
+      <li>
+        <em>Fingers</em>. Where you can choose the length of characters set and width of the typing words.
+      </li>
+      <li>
+        <em>Free</em>. Where you can specify which characters you want to train and set width of the typing words.
+      </li>
     </ul>
-  </Fragment>
-  /* eslint-enable react/jsx-one-expression-per-line */
+
+    Right now <em>Touch to type</em> is free app. If you have question please send it to <a href="mailto:info@touchtotype.com">info@touchtotype.com </a>.
+  </section>
+  /* eslint-enable react/jsx-one-expression-per-line, max-len */
 );
 
 export default CSSModules(Block, styles);
