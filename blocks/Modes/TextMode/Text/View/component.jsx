2e3e2e10d7366aca8633394dfa6088babe21311d
uaNikita <nikita.zhukov@gmail.com>
2018-11-09 18:20:33 +0200

    refactoring to increase render speed

--- blocks/Modes/TextMode/Text/View/component.jsx
+++ blocks/Modes/TextMode/Text/View/component.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
 
@@ -6,52 +6,64 @@ import { convertTextToHtml } from 'Utils';
 
 import styles from './text-view.module.styl';
 
-const Block = (props) => {
-  const {
-    id,
-    typed,
-    last,
-    selectedId,
-    selectText,
-    match: {
-      url,
-    },
-  } = props;
+class Block extends Component {
+  constructor(props) {
+    super(props);
 
-  let button = {
-    props: {
-      onClick: () => selectText(id),
-    },
-    text: 'Select',
-  };
+    const {
+      typed,
+      last,
+    } = props;
 
-  if (id === selectedId) {
-    button = {
-      props: {
-        disabled: true,
-      },
-      text: 'Selected',
-    };
+    this.text = typed + last;
   }
 
-  const text = typed + last;
+  render() {
+    const {
+      props: {
+        id,
+        selectedId,
+        selectText,
+        match: {
+          url,
+        },
+      },
+    } = this;
 
-  return (
-    <Fragment>
-      <div styleName="actions">
-        <button type="button" className="button" {...button.props}>
-          {button.text}
-        </button>
+    let button = {
+      props: {
+        onClick: () => selectText(id),
+      },
+      text: 'Select',
+    };
 
-        <Link to={`${url}/edit`} className="button">
-          Edit
-        </Link>
-      </div>
+    if (id === selectedId) {
+      button = {
+        props: {
+          disabled: true,
+        },
+        text: 'Selected',
+      };
+    }
 
-      {/* eslint-disable-next-line react/no-danger */}
-      <p dangerouslySetInnerHTML={{ __html: convertTextToHtml(text) }} />
-    </Fragment>
-  );
-};
+    // todo: move convertTextToHtml to PureFunction
+    return (
+      <Fragment>
+        <div styleName="actions">
+          <button type="button" className="button" {...button.props}>
+            {button.text}
+          </button>
+
+          <Link to={`${url}/edit`} className="button">
+            Edit
+          </Link>
+        </div>
+
+        {/* eslint-disable-next-line react/no-danger */}
+        <p dangerouslySetInnerHTML={{ __html: convertTextToHtml(this.text) }} />
+      </Fragment>
+    );
+  }
+}
 
 export default CSSModules(Block, styles);
--- blocks/Modes/TextMode/Texts/component.jsx
+++ blocks/Modes/TextMode/Texts/component.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
 import classNames from 'classnames';
@@ -10,60 +10,72 @@ import GeneralModeButton from '../../GeneralModeButton/container';
 
 import styles from './texts.module.styl';
 
-const Block = (props) => {
-  const {
-    texts,
-    selectedId,
-    match: {
-      url,
-    },
-  } = props;
+class Block extends Component {
+  constructor(props) {
+    super(props);
 
-  let addTextLink;
-  let textEls = <Loader styleName="loader" size="30" />;
+    const {
+      typed,
+      last,
+    } = props;
 
-  if (texts) {
-    textEls = texts.map(({ id, content }) => {
-      const className = classNames('text', {
-        text_selected: id === selectedId,
-      });
-
-      return (
-        <Link key={id} to={`${url}/${id}`} styleName={className}>
-          {/* eslint-disable-next-line react/no-danger */}
-          <p dangerouslySetInnerHTML={{ __html: convertTextToHtml(content) }} />
-        </Link>
-      );
-    });
-
-    if (texts.length < 10) {
-      addTextLink = (
-        <Link to={`${url}/add`}>
-          Add new text
-        </Link>
-      );
-    }
-    else {
-      addTextLink = (
-        <p>
-          Your can have maximum 10 texts
-        </p>
-      );
-    }
+    this.text = typed + last;
   }
 
-  return (
-    <Fragment>
-      <div styleName="actions">
-        <GeneralModeButton toMode="text" />
+  render() {
+    const {
+      props: {
+        texts,
+        selectedId,
+        match: {
+          url,
+        },
+      },
+    } = this;
 
-        {addTextLink}
-      </div>
+    let addTextLink;
+    let textEls = <Loader styleName="loader" size="30" />;
 
-      {textEls}
-    </Fragment>
-  );
-};
+    if (texts) {
+      textEls = texts.map(({ id, content }) => {
+        const className = classNames('text', {
+          text_selected: id === selectedId,
+        });
+
+        // todo move convertTextToHtml to somewhere
+        return (
+          <Link key={id} to={`${url}/${id}`} styleName={className}>
+            {/* eslint-disable-next-line react/no-danger */}
+            <p dangerouslySetInnerHTML={{ __html: convertTextToHtml(content) }} />
+          </Link>
+        );
+      });
+
+      if (texts.length < 10) {
+        addTextLink = (
+          <Link to={`${url}/add`}>
+            Add new text
+          </Link>
+        );
+      }
+      else {
+        addTextLink = <p>Your can have maximum 10 texts</p>;
+      }
+    }
+
+    return (
+      <Fragment>
+        <div styleName="actions">
+          <GeneralModeButton toMode="text" />
+
+          {addTextLink}
+        </div>
+
+        {textEls}
+      </Fragment>
+    );
+  }
+}
 
 export default CSSModules(Block, styles, {
   allowMultiple: true,
