b50752552e963ff90db3d9b98843e739e109676a
uaNikita <nikita.zhukov@gmail.com>
2018-11-13 11:55:07 +0200

    add multi language for getSyllables

--- server/getSyllables.js
+++ server/getSyllables.js
@@ -1,71 +1,111 @@
 const _ = require('lodash');
 const fs = require('fs');
+const path = require('path');
 const cheerio = require('cheerio');
 const fetch = require('isomorphic-fetch');
 
 const requestFinishDate = 10 * 60 * 1000 + Date.now(); // 10 min from now
-const requestDelay = 1 * 1000; // 5 sec
-const requestedUrls = [];
+const requestDelay = 1 * 500; // 5 sec
+
+const languages = [
+  {
+    subdomain: 'en',
+    regex: /^[a-z]{2}$/
+  },
+  {
+    subdomain: 'ru',
+    regex: /^[ёа-я]{2}$/
+  }
+];
+const requestedUrls = {};
 const syllables = [];
+_.each(languages, ({ subdomain }) => {
+  requestedUrls[subdomain] = [];
 
-console.time('duration');
+  syllables.push({
+    language: subdomain,
+    data: {},
+  });
+})
 
-const get = () =>
-  fetch('https://en.wikipedia.org/wiki/Special:Random')
-    .then(function(response) {
-      if (response.ok) {
-        if (requestedUrls.includes(response.url)) {
-          get();
+const repeatIfNeeded = func => {
+  if ((Date.now() + requestDelay) <= requestFinishDate) {
+    setTimeout(func, requestDelay);
 
-          throw new Error('URL already was requested');
-        }
-        else {
-          console.log(`url - \'${response.url}\'`);
+    return true;
+  }
+};
 
-          requestedUrls.push(response.url);
+const get = () => {
+  const requests = languages.map(({ subdomain }) => fetch(`https://${subdomain}.wikipedia.org/wiki/Special:Random`));
 
-          return response.text();
-        }
-      }
+  Promise
+    .all(requests)
+    .then(responses => (
+      Promise.all(responses.map((response, i) => {
+        if (response.ok) {
+          const requestedLanguageUrls = requestedUrls[languages[i].subdomain]
 
-      throw new Error('Bad response from server');
-    })
-    .then(data => {
-      const $ = cheerio.load(data);
-      const characters = $('#mw-content-text p').text().split('');
+          if (requestedLanguageUrls.includes(response.url)) {
+            return Promise.reject('URL already was requested');
+          }
+          else {
+            console.log(`url - '${response.url}'`);
 
-      characters.forEach((character, i) => {
-        const nextCharacter = characters[i + 1];
+            requestedLanguageUrls.push(response.url);
 
-        if (nextCharacter) {
-          const syllable = (character + nextCharacter).toLowerCase();
-
-          if (/^[a-z]{2}$/.test(syllable)) {
-            const obj = _.find(syllables, { syllable });
-
-            if (obj) {
-              obj.count += 1;
-            }
-            else {
-              syllables.push({
-                syllable,
-                count: 1,
-              });
-            }
+            return response.text();
           }
         }
+
+        return Promise.reject('Bad response from server');
+      }))
+    ))
+    .then(data => {
+      data.forEach((html, idx) => {
+        const $ = cheerio.load(html);
+        const characters = $('#mw-content-text p').text().split('');
+        const language = languages[idx];
+
+        characters.forEach((character, i) => {
+          const nextCharacter = characters[i + 1];
+
+          if (nextCharacter) {
+            const syllable = (character + nextCharacter).toLowerCase();
+
+            if (language.regex.test(syllable)) {
+              const obj = _.find(syllables, {
+                language: language.subdomain,
+              })
+
+              if (obj.data[syllable]) {
+                obj.data[syllable] += 1;
+              }
+              else {
+                obj.data[syllable] = 1
+              }
+            }
+          }
+        });
       });
-      
+
       console.log('parsed');
 
-      if ((Date.now() + requestDelay) <= requestFinishDate) {
-        setTimeout(get, requestDelay);
+      if (!repeatIfNeeded(get)) {
+        const pathToJSON = path.join('constants/syllables.json')
+
+        const syllablesJSON = syllables.reduce((a, b) => (a[b.language] = b.data, a), {});
+
+        fs.writeFileSync(pathToJSON, JSON.stringify(syllablesJSON), 'utf8');
+
+        console.log(`result is saved in '${pathToJSON}'`);
       }
-      else {
-        fs.writeFile('constants/syllables.json', JSON.stringify(syllables), 'utf8');
-        
-        console.log('result is saved in \'constants/myjsonfile.json\'');
-      }
+    })
+    .catch(error => {
+      console.error(error);
+
+      repeatIfNeeded(get);
     });
+}
 
 get();
