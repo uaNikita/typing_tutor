2bae83dd7b36e589762dbbaf0adb26d01dfbcd03
Unknown <nikita.zhukov@gmail.com>
2018-12-06 23:20:36 +0200

    refactor menu links again

--- blocks/Header/component.jsx
+++ blocks/Header/component.jsx
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter, Link, Route } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
-
-import { modes, other } from 'Constants/navigation';
 
 import NavLink from 'Blocks/NavLink';
 import Logo from 'Blocks/Logo/component';
@@ -10,60 +8,30 @@ import UserMenu from './UserMenu/container';
 
 import styles from './header.module.styl';
 
-class Block extends Component {
-  generateLinks = (data) => {
-    const {
-      props: {
-        location,
-      },
-    } = this;
+const Block = ({ location: { pathname } }) => (
+  <div styleName="root">
+    <div styleName="left">
+      {pathname === '/'
+        ? <Logo />
+        : <Link styleName="home" className="far fa-keyboard" to="/" />}
 
-    return data.map((link) => {
-      const {
-        pathname,
-        text,
-      } = link;
+      <nav styleName="items">
+        <span styleName="modes-title">I type</span>
 
+        <NavLink styleName="item" activeClassName={styles.item_selected} to="/mode/learning">Learning</NavLink>
+        <NavLink styleName="item" activeClassName={styles.item_selected} to="/mode/text">Text</NavLink>
+      </nav>
+    </div>
 
-      return <NavLink styleName="item" activeClassName={styles.item_selected} to={pathname}>{text}</NavLink>
-    });
-  };
+    <div styleName="right">
+      <NavLink styleName="item" activeClassName={styles.item_selected} to="/options">Options</NavLink>
 
-  render() {
-    const {
-      props: {
-        location: {
-          pathname,
-        },
-      },
-    } = this;
+      <NavLink styleName="item" activeClassName={styles.item_selected} to="/statistic">Statistic</NavLink>
 
-    return (
-      <div styleName="root">
-        {pathname === '/'
-          ? <Logo />
-          : <Link styleName="home" className="far fa-keyboard" to="/" />}
-
-        <div styleName="actions">
-          <nav styleName="items">
-            {/* todo: what can I do with menu wrap with some bg */}
-            <span styleName="modes-title">Modes:</span>
-
-            <NavLink styleName="item" activeClassName={styles.item_selected} to="/mode/learning">Learning</NavLink>
-            <NavLink styleName="item" activeClassName={styles.item_selected} to="/mode/text">Text</NavLink>
-            |
-            <NavLink styleName="item" activeClassName={styles.item_selected} to="/options">Options</NavLink>
-
-            ...................
-            <NavLink styleName="item" activeClassName={styles.item_selected} to="/statistic">Statistic</NavLink>
-          </nav>
-
-          <Route path="/" component={UserMenu} />
-        </div>
-      </div>
-    );
-  }
-}
+      <Route path="/" component={UserMenu} />
+    </div>
+  </div>
+);
 
 export default withRouter(CSSModules(Block, styles, {
   allowMultiple: true,
