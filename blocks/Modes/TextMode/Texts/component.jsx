fe0b6e0a263c424afc39b1d9b80385248d778255
Nikita Zhukov <nikita.zhukov@gmail.com>
2018-12-11 23:05:06 +0200

    remove unneeded shouldComponentUpdate

--- blocks/Modes/TextMode/Texts/component.jsx
+++ blocks/Modes/TextMode/Texts/component.jsx
@@ -1,6 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import _ from 'lodash';
 import CSSModules from 'react-css-modules';
 import classNames from 'classnames';
 
@@ -9,92 +8,75 @@ import PureString from 'Blocks/PureString';
 
 import styles from './texts.module.styl';
 
-class Block extends Component {
-  // todo: remove this
-  shouldComponentUpdate = (nextProps) => {
-    const propsToPick = ['texts', 'selectedId'];
-    const pikedProps = _.pick(this.props, propsToPick);
-    const pikedNextProps = _.pick(nextProps, propsToPick);
+const Block = (props) => {
+  const {
+    texts,
+    selectedId,
+    match: {
+      url,
+    },
+  } = props;
 
-    if (_.isEqual(pikedProps, pikedNextProps)) {
-      return false;
-    }
+  let addTextLink;
+  let textEls = <Loader styleName="loader" size="30" />;
 
-    return true;
-  };
+  if (texts) {
+    textEls = texts.map(({ id, content }) => {
+      let className = 'text';
 
-  render() {
-    const {
-      props: {
-        texts,
-        selectedId,
-        match: {
-          url,
-        },
-      },
-    } = this;
-
-    let addTextLink;
-    let textEls = <Loader styleName="loader" size="30" />;
-
-    if (texts) {
-      textEls = texts.map(({ id, content }) => {
-        let className = 'text';
-
-        if (selectedId) {
-          className = classNames(className, {
-            text_selected: id === selectedId,
-          });
-        }
-
-        return (
-          <Link key={id} to={`${url}/${id}`} styleName={className}>
-            <PureString
-              string={content}
-              tag="p"
-              html
-            />
-          </Link>
-        );
-      });
-
-      if (texts.length < 10) {
-        addTextLink = (
-          <Link to={`${url}/add`}>Add new text</Link>
-        );
+      if (selectedId) {
+        className = classNames(className, {
+          text_selected: id === selectedId,
+        });
       }
-      else {
-        addTextLink = <p>Your can have maximum 10 texts</p>;
-      }
+
+      return (
+        <Link key={id} to={`${url}/${id}`} styleName={className}>
+          <PureString
+            string={content}
+            tag="p"
+            html
+          />
+        </Link>
+      );
+    });
+
+    if (texts.length < 10) {
+      addTextLink = (
+        <Link to={`${url}/add`}>Add new text</Link>
+      );
     }
-
-    return (
-      <Fragment>
-        <div styleName="actions">
-          <p>
-            {selectedId && (
-              <Fragment>
-                <button
-                  type="button"
-                  className="button"
-                  styleName="button"
-                >
-                  Start
-                </button>
-                typing last selected text
-              </Fragment>
-            )}
-
-          </p>
-
-          {addTextLink}
-        </div>
-
-        {textEls}
-      </Fragment>
-    );
+    else {
+      addTextLink = <p>Your can have maximum 10 texts</p>;
+    }
   }
-}
+
+  return (
+    <Fragment>
+      <div styleName="actions">
+        <p>
+          {selectedId && (
+            <Fragment>
+              <button
+                type="button"
+                className="button"
+                styleName="button"
+              >
+                Start
+              </button>
+              typing last selected text
+            </Fragment>
+          )}
+
+        </p>
+
+        {addTextLink}
+      </div>
+
+      {textEls}
+    </Fragment>
+  );
+};
 
 export default CSSModules(Block, styles, {
   allowMultiple: true,
