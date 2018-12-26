577e477eb6aa285cbcc27e9f103238e8a1082b65
Nikita Zhukov <nikita.zhukov@gmail.com>
2018-12-26 19:34:23 +0200

    add options description

--- blocks/Help/component.jsx
+++ blocks/Help/component.jsx
@@ -11,14 +11,16 @@ const Block = () => (
   <section styleName="help">
     <h1>Help</h1>
     <em>Touch to type</em> is app which help you to typing text faster with smaller amount of typos. You can use it without registration, but in this case your progress will be kept in your browser. So if for example you
-    move to another browser or clear your browser data you lose all your statistic and choosed options. If you want to prevent this, please create your account with registration <Link to="/authorization/sign-up">page</Link>.
+    move to another browser or clear your browser data you lose all your statistic and choosed options. If you want to prevent this, please create your account with
+    registration <Link to="/authorization/sign-up">page</Link>.
 
     <h2>Modes</h2>
     <p>The app has two modes:</p>
 
     <ul>
       <li>
-        <strong>Syllable</strong>. Where you can train various most common letter combinations. The app scans Wikipedia searchs on the most frequently encountered combinations of letters and on their basis generates a string for training. This mode has two different submodes:
+        <strong>Syllable</strong>. Where you can train various most common letter combinations. The app scans Wikipedia searchs on the most frequently encountered combinations of letters and on their basis generates a
+        string for training. This mode has two different submodes:
         <ul>
           <li>
             <em>Fingers</em>. Where you can choose the length of characters set and width of the typing words.
@@ -34,7 +36,7 @@ const Block = () => (
     </ul>
 
     <h2>Options</h2>
-    {/* todo: write about options here */}
+    <p>Here you can change your <i>Keyboard layout</i> if needed.</p>
 
     <h2>Statistic</h2>
     <p>App starts to save statitic after 10 seconds from start typing of any of modes. You can see your statistic in different charts on the corresponding page.</p>
