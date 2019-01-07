5fbd18a36b1a9cb2bbeb3fea59c11f45df4fdfad
Unknown <nikita.zhukov@gmail.com>
2019-01-07 14:48:23 +0200

    add new code for Game mode

--- blocks/Home/GameArea/component.jsx
+++ blocks/Home/GameArea/component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import _ from 'lodash';
 import CSSModules from 'react-css-modules';
 
@@ -10,6 +10,8 @@ import ContentArea from '../ContentArea';
 import styles from './game-area.module.styl';
 
 class Block extends ContentArea {
+  area = React.createRef();
+
   state = {
     words: [],
   };
@@ -27,13 +29,18 @@ class Block extends ContentArea {
 
   step = 20;
 
+  slots = 6;
+
+  wordHeight = 26;
+
   start = () => {
     const go = () => {
       this.setState(({ words }) => ({
-        words: words.map(w => ({
-          ...w,
-          x: w.x - this.step,
-        })),
+        words: words.map(word => {
+          word.left -= this.step;
+
+          return word;
+        }),
       }));
 
       setTimeout(go, 1000);
@@ -42,17 +49,44 @@ class Block extends ContentArea {
     go();
   };
 
-  addWord = () => {
-    const index = _.random(0, this.nouns.length - 1);
-    const word = this.nouns[index];
-    this.nouns.splice(index, 1);
+  getFreeRandomSlot = () => {
+    const words = this.area.current.querySelectorAll(`.${styles.word}`);
 
-    this.setState(({ words }) => ({
-      words: words.push({
-        x: this.areaWidth,
+    const slots = _.range(0, this.slots);
+
+    if (words) {
+      Array.from(words).map(word => {
+        const extremeRightEdge = word.offsetLeft + word.offsetWidth + this.step;
+
+        if (extremeRightEdge > this.areaWidth) {
+          const occupiedLine = word.offsetTop / this.wordHeight;
+
+          _.pull(slots, occupiedLine);
+        }
+      });
+    }
+
+    return _.sample(slots);
+  };
+
+  addWord = () => {
+    const allWords = Object.keys(this.nouns);
+    const index = _.random(0, allWords.length - 1);
+    const word = allWords[index];
+
+    delete this.nouns[word];
+
+    this.setState(({ words }) => {
+      words.push({
+        top: this.getFreeRandomSlot() * this.wordHeight,
+        left: this.areaWidth,
         word,
-      })
-    }));
+      });
+
+      return {
+        words,
+      };
+    });
   };
 
   componentDidMount = () => {
@@ -64,11 +98,9 @@ class Block extends ContentArea {
     document.addEventListener('keydown', this.keyDownHandler);
     document.addEventListener('keypress', this.keyPressHandler);
 
-
     this.start();
 
     setInterval(this.addWord, 5000);
-
 
     // updateCharToType();
   };
@@ -94,12 +126,24 @@ class Block extends ContentArea {
     } = this;
 
     return (
-      <div styleName="root">
-        <p styleName="area">
-          {words.map(({ x, word }) => (
+      <Fragment>
+
+        <div styleName="level">
+          Easy
+          <div styleName="level-line">
+            <div styleName="level-pointer" style={{
+              width: '0%',
+            }} />
+          </div>
+          Hard
+        </div>
+
+
+        <p styleName="area" ref={this.area}>
+          {words.map(({ top, left, word }) => (
               <span
                 key={word}
-                style={{ top: 70, left: x }}
+                style={{ top, left }}
                 styleName="word"
               >
                 {word}
@@ -119,7 +163,7 @@ class Block extends ContentArea {
             hiddenChars
           />
         </p>
-      </div>
+      </Fragment>
     );
   }
 }
