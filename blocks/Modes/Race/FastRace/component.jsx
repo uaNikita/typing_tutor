528f57627a6ea90b2aef4bfc44fd48e558ad29d2
nikita.zhukov@gmail.com <nikita.zhukov@gmail.com>
2019-01-24 17:50:40 +0200

    add styles to FastRace component

--- blocks/Modes/Race/FastRace/component.jsx
+++ blocks/Modes/Race/FastRace/component.jsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import CSSModules from 'react-css-modules';
+
+import styles from './fast-race.module.styl';
 
 class Block extends Component {
   speedUp = () => {
@@ -14,15 +16,19 @@ class Block extends Component {
     } = this;
 
     return (
-      <button
-        type="button"
-        className="button"
-        onClick={start}
-      >
-        Start
-      </button>
+      <Fragment>
+        <p styleName="info">Here you can start fast races with random partners or bots</p>
+
+        <button
+          type="button"
+          className="button"
+          onClick={start}
+        >
+          Start
+        </button>
+      </Fragment>
     );
   }
 }
 
-export default CSSModules(Block);
+export default CSSModules(Block, styles);
